ddc8e80b1adabf988fda843adf69408079da0dca
serenaliu628 <yl2904@columbia.edu>
2017-05-09 15:32:38 -0400

    Update page_walk.cu

--- page_walk.cu
+++ page_walk.cu
@@ -7,11 +7,12 @@
 
   Created by Serean Liu and Raphael Norwitz.
 
-
-Note: Needs compute capability >= 2.0, so compile with:
 Usage:
-nvcc page_walk1.cu -arch=compute_20 -code=sm_20,compute_20 -o page_walk1.out
-./page_walk1.out -n 4000 2 2 2 2 2
+
+nvcc page_walk1.cu -o page_walk1.out
+
+./page_walk1.out -n <total addresses> <intermediate table parameters>
+(eg, ./page_walk1.out -n 400 2 3 2 4)
  ************************************************************************************/
 
 
@@ -55,6 +56,7 @@ struct trans_thread {
 	int max;
 };
 
+//function called by CUDA kernel
 __host__ __device__ int translate_cpu(struct trans_thread *trans) {
 	unsigned long *gpu_ptr = (unsigned long *) trans->curr_table;
 	while(trans->curr < trans->max-1) {
@@ -64,6 +66,7 @@ __host__ __device__ int translate_cpu(struct trans_thread *trans) {
 	return (int) *((int *) gpu_ptr + trans->offset[trans->curr]); // ((void *) trans->curr_table + trans->offset[trans->max-1]);
 }
 
+//CPU counterpart 
 int translate_cpu2(struct trans_thread *trans) {
 	//void **c = trans->curr_table;
 	while(trans->curr < trans->max-1) {
@@ -87,38 +90,21 @@ __global__ void gpu_run_time(struct trans_thread *trans, int addresses) {
 	int subthread = threadIdx.z*(blockDim.x * blockDim.y) + threadIdx.y*blockDim.x + threadIdx.x;
 	// assign overall id/index of the thread
 	int idx = myblock * blocksize + subthread;
-
-
-
+	
 	if(idx < addresses) {
-
-		// printf("Hello world! My block index is (%d,%d) [Grid dims=(%d,%d)], 3D-thread index within block=(%d,%d,%d) => \
-		thread index=%d\n", blockIdx.x, blockIdx.y, gridDim.x, gridDim.y, threadIdx.x, threadIdx.y, threadIdx.z, idx);
-		//translate_cpu(&trans[idx]);
-		/*while((*(trans+idx)).curr < (*(trans+idx)).max-1) {
-		  (*(trans+idx)).curr_table = (void **) (*(trans+idx)).curr_table[(*(trans+idx)).offset[(*(trans+idx)).curr]];
-		  (*(trans+idx)).curr++;
-		  }*/
+		
 		translate_cpu(&trans[idx]);
-		// printf("Hello world!.\n");
 	}
 }
 
 
 // CPU analog for speed comparison
 float cpu_run_time(struct trans_thread *trans, int addresses) {
-	//struct timeval start_time, stop_time;
-
-	//gettimeofday(&start_time, NULL);
-
+	
 	for(int i = 0; i < addresses; i++) {
-		translate_cpu2(&trans[i]);
-		//printf("Hello world!.\n");
+		translate_cpu2(&trans[i]);	
 	}
 
-	//gettimeofday(&stop_time, NULL);
-
-	//long time_diff = (stop_time.tv_usec)-(start_time.tv_usec);
 	return 0;
 }
 
@@ -130,6 +116,7 @@ double read_timer() {
 	return end.tv_sec+1.e-6*end.tv_usec;
 }
 
+//sequential phase 1: construct the table 
 int construct_table(void *table, int *levels, int num_levels) {
 	int i, j, level_size = 1;
 	void **table_ptr = (void **) table;
@@ -140,8 +127,6 @@ int construct_table(void *table, int *levels, int num_levels) {
 	{
 		level_size *= levels[i];
 
-		// hideous but best way I could find to get next level
-		//level_ptr = (void **) (table + (level_size)+ table_ptr);
 		level_ptr = (unsigned long **) table + level_size + (((unsigned long *)table_ptr - (unsigned long *) table)/(sizeof(unsigned long *)));
 
 		fprintf(stderr, "level_size: %d, level_ptr: %d, table_ptr: %d\n", level_size, (level_ptr- (unsigned long **) table) / sizeof(void *), (unsigned long **) table_ptr -  (unsigned long **) table);
@@ -155,20 +140,18 @@ int construct_table(void *table, int *levels, int num_levels) {
 	assert((intptr_t )table_ptr - (intptr_t )table < max_table);
 
 
-	// set last level of page table to garbage; for our purposes
-	// it doesn't matter
+	// set last level of page table to garbage; 
 	for(i = 0; i < level_size * levels[num_levels-1]; i++) {
 		*table_ptr = (unsigned long *) i;
 		table_ptr++;
 	}
-
-
 	assert((intptr_t )table_ptr - (intptr_t )table == max_table);
 	// return number of entries at the lowest level of the
 	// page table
 	return levels[num_levels-1] * level_size;
 }
 
+//sequential phase 2: generate struct trans_thread for each address
 struct trans_thread *gen_addresses(int num_addr, int levels, int *level_sizes, void **pgd)
 {
 	int i,j;
@@ -199,7 +182,6 @@ struct trans_thread *gen_addresses(int num_addr, int levels, int *level_sizes, v
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv) {
 
-	//void **pg_table; //host page table
 	void **d_pg_table; //device page table
 
 	int i, j, table_size = 0, level_size = 1,
@@ -208,12 +190,12 @@ int main(int argc, char** argv) {
 
 	int level_sizes[levels];
 	//struct trans_thread *sample;
-	struct trans_thread *h_new_threads;
-	struct trans_thread *d_new_threads;
+	struct trans_thread *h_new_threads; //host
+	struct trans_thread *d_new_threads; //device
 
 	cudaError_t cudaStat;
 
-
+	//start of the program
 	printf("===============================================================================\n");
 	srand(time(NULL));
 
@@ -228,14 +210,17 @@ int main(int argc, char** argv) {
 		table_size += level_size;
 	}
 
-	// fixed block dimensions (1024x1x1 threads)
+	//tuning the parameters of the device 
+	// fixed block size (512 x1x1 threads)
 	const dim3 blockSize(BLOCK_D1, BLOCK_D2, BLOCK_D3);
 	// determine number of blocks we need for a given problem size
 	int tmp = ceil(pow(total_addresses/BLOCK_D1, 0.5));
 	printf("Grid dimension is %i x %i\n", tmp, tmp);
+	//grid size
 	dim3 gridSize(tmp, tmp, 1);
-
+        //total number of threads 
 	int nthreads = BLOCK_D1*BLOCK_D2*BLOCK_D3*tmp*tmp;
+	//threads number legitimacy check
 	if (nthreads < total_addresses){
 		printf("\n============ NOT ENOUGH THREADS TO COVER total addresses=%d ===============\n\n",total_addresses);
 	} else {
@@ -264,6 +249,7 @@ int main(int argc, char** argv) {
 		printf ("device memory allocation succeeded.\n");
 	}
 
+	/* --------------------------- sequential code on CPU; phase 1 ------------------------------*/
 	//number of entries at the lowest level of the page table
 	//number of translatable addresses
 	printf ("now construct the page table on the host.\n");
@@ -275,7 +261,7 @@ int main(int argc, char** argv) {
 	cudaDeviceSynchronize();
 	double tInit = read_timer();
 
-	// copy input data to the GPU
+	/* --------------------------- copy the page table from CPU to the GPU ------------------------------*/
 	cudaStat = cudaMemcpy(d_pg_table, pg_table, sizeof(void *) * table_size, cudaMemcpyHostToDevice);
 
 	printf("Memory Copy for page table from Host to Device");
@@ -288,11 +274,12 @@ int main(int argc, char** argv) {
 	cudaDeviceSynchronize();
 	double tTransferToGPU_pgtable = read_timer();
 
+	/* --------------------------- sequential code on CPU; phase 2 ------------------------------*/
 	h_new_threads = gen_addresses(total_addresses, levels, level_sizes, pg_table);
 	cudaDeviceSynchronize();
 	double tInit2 = read_timer();
 
-	//Copy the struct to device memory
+	/* --------------------------- copy the trans_threads from CPU to the GPU ------------------------------*/
 	cudaStat = cudaMalloc( (void**) &d_new_threads, sizeof(struct trans_thread) * total_addresses) ;
 	if (cudaStat != cudaSuccess){
 		printf("device memory allocation for d_new_threads failed.\n");
@@ -313,19 +300,9 @@ int main(int argc, char** argv) {
 
 	cudaDeviceSynchronize();
 	double tTransferToGPU_threads = read_timer();
-
+	
+        /* --------------------------- parallel code on GPU: kernel ------------------------------*/
 	gpu_run_time<<<gridSize, blockSize>>>(d_new_threads, total_addresses);
-
-	for(i = 0; i < table_size; i++) {
-		fprintf(stderr, "address number: %d, address intermediate value: %d, final value: %d, address: %d\n", i,  ((void **) pg_table[i] - pg_table) / sizeof(void *), pg_table[i], pg_table+i);
-	}
-
-	for (i = 0; i <  table_size - table_lowest_addresses; i++)
-	{
-		fprintf(stderr, "address %d, points to %d\n", pg_table + i, pg_table[i]);
-	}
-
-
 
 	cudaError_t cudaerr = cudaDeviceSynchronize();
 	if (cudaerr){
@@ -339,32 +316,18 @@ int main(int argc, char** argv) {
 	double gpu_time = read_timer();
 	printf("GPU done!\n");
 
-	/*cudaStat = cudaMemcpy( h_new_threads, d_new_threads, sizeof(struct trans_thread) * total_addresses, cudaMemcpyDeviceToHost);
-	//cudaMemcpy(cpu_vals, gpu_vals, N, cudaMemcpyDeviceToHost);
-	printf("Memory Copy from Device to Host ");
-	if (cudaStat){
-	printf("failed.\n");
-	} else {
-	printf("successful.\n");
-	}
-	cudaCheckErrors("cudaMemcpy fail");
-
-	cudaDeviceSynchronize();
-	double tTransferFromGPU = read_timer();*/
-
-
+        /* --------------------------- sequential code on CPU; phase 3 ------------------------------*/
 	printf("now do calculation on CPU for comparison!\n");
 	cpu_run_time(h_new_threads, total_addresses);
 	double cpu_time = read_timer();
-
 	fprintf(stderr, "The CPU took %lu microseconds to compute %d addresses. ""For a table of depth %d.\n",	cpu_time - gpu_time , total_addresses, levels);
 
+	/* --------------------------- sequential code on CPU; phase 4 ------------------------------*/
 	printf("Timing results for n = %d\n", total_addresses);
         //printf("page table Transfer to GPU time: %f\n", tTransferToGPU_pgtable - tInit);
         //printf("threads Transfer to GPU time: %f\n", tTransferToGPU_threads - tInit2);
         printf("Calculation time (GPU): %f\n", gpu_time - tTransferToGPU_threads);
         //printf("Transfer from GPU time: %f\n", tTransferFromGPU - gpu_time);
-
 	printf("Calculation time (CPU): %f\n", cpu_time - gpu_time);
 
 
